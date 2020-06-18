c5ec6d861192ec831285dc4c135b6321aae225d9
mvagulina <marinka_2001@mail.ru>
2020-06-18 13:13:11 +0300

    добавлены параметры в командную строку

--- Students/Marina/lessons/2/app/index.js
+++ Students/Marina/lessons/2/app/index.js
@@ -1,28 +1,9 @@
-const input = require("./components/textInput");
-const checker = require("./components/checker");
-const logger = require("./utils/logger");
-const generator = require("./components/generator");
+const game = require("./components/game");
+const minimist = require("minimist");
 
-let game = async function (max_number, max_attempts) {
-  let pc = generator(max_number),
-    win = false,
-    attempts = [];
-
-  console.log(
-    `Компьютер загадал число. Угадай его. У тебя ${max_attempts} попытки.`
-  );
-  while (!win && attempts.length < max_attempts) {
-    await input(max_number, attempts.length + 1).then((text) => {
-      attempts.push(text);
-      if (checker(text, pc)) {
-        console.log("Ты угадал");
-        win = true;
-      } else {
-        console.log("Не верно");
-      }
-    });
-  }
-  logger(pc, attempts, win);
-};
-
-game(10, 3);
+let argv = minimist(process.argv.slice(2), {
+  alias: { number: "n", attempts: "a" },
+});
+let max_number = typeof argv.number === "number" && argv.number > 1 ? argv.number : 10;
+let max_attempts = typeof argv.attempts === "number" && argv.attempts > 0 ? argv.attempts : 3;
+game(max_number, max_attempts);
