e6ae827988305cef6266f5c1801f37e5b1ba2869
Agregat <ikrasnobai@yandex.ru>
2020-06-20 13:28:25 +0300

    hw2 fix

--- Students/Ivan_Krasnobaev/lessons/2/hw/index.js
+++ Students/Ivan_Krasnobaev/lessons/2/hw/index.js
@@ -1,6 +1,8 @@
 const readline = require('readline');
 const game = require('./components/game');
 const generate = require('./components/generator');
+const checkInput = require('./components/checker');
+const log = require('./utils/logger');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,7 +10,9 @@ const rl = readline.createInterface({
 });
 
 let quest = 'Быки и коровы\nЗагадано 4-х значное число\n';
+let attempts = 1;
 const number = Array();
+
 for (let i = 0; i < 4; i++) {
     let num = generate();
     while (number.indexOf(num) !== -1) {
@@ -16,17 +20,32 @@ for (let i = 0; i < 4; i++) {
     }
     number.push(num);
 }
-//console.log(number);
+
+console.log(number);
+// прошу прощения, что сделано через рекурсиюю вы на неё ругались.
+// не хватает времени переделать ¯\_(ツ)_ /¯
 function ask(q) {
     rl.question(q, (answer) => {
-        if (game(answer,number) === true) {
+        // проверка ввода
+        if (!checkInput(answer)) {
+            rl.question('Некорректный ввод! Хотите выйти? y/n\n', (ans) => {
+                if (ans === 'y') {
+                    log(number, attempts, false);
+                    rl.close();
+                } else ask('Угадайте 4-х значное число\n');
+            });
+        }
+        // проверка ответа
+        if (game(answer, number) === true) {
+            log(number, attempts);
             rl.close();
-            return true;
         } else {
+            attempts++;
             quest = game(answer,number);
             ask(quest);
         }
     });
 }
 
+// запуск игры
 ask(quest);
