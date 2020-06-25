1d6a336d3f0053c6ff2cd67d33f045ae189e31aa
ivnikonGB <ivnikon@yandex.ru>
2020-06-25 10:04:27 +0300

    lesson_3 client side done

---
+++ Students/Nikolay Ivanov/lessons/lesson_3/client/src/components/OutputField.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+        <OutputItem
+           v-for="attempt in attempts"
+            v-bind:key="attempt"
+            v-bind:attempt="attempt"
+        />
+        
+    </ul>
+</template>
+
+<script>
+import OutputItem from '@/components/OutputItem'
+export default {
+    props: ['attempts'],
+    components: {
+        OutputItem
+    }
+}
+</script>
+<style scoped>
+    ul {
+        list-style: none;
+    }
+</style>
