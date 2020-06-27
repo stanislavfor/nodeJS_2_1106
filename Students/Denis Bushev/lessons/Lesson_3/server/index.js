44f31429968dce7cefd959f2ac505e67ff210e8e
Jestric-sys <Poltos_94@mail.ru>
2020-06-27 03:56:07 +0300

    Lesson_4

--- Students/Denis Bushev/lessons/Lesson_3/server/index.js
+++ Students/Denis Bushev/lessons/Lesson_3/server/index.js
@@ -1,49 +1,26 @@
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
-const path = require('path');
+const logger = require('./utils/logger');
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, '../public', req.url === '/' ? 'index.html' : req.url);
-    const ext = path.extname(filePath);
-    let contentType = 'text/html';
+const PORT = 8080;
+const server = express();
+server.use(express.json());
 
-    switch (ext) {
-        case '.css':
-            contentType = 'text/css'
-            break
-        case '.js':
-            contentType = 'text/javascript'
-            break
-        default:
-            contentType = 'text/html'
-    }
+server.use('/', express.static('./public'));
 
-    if (!ext) {
-        filePath += '.html'
-    };
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            fs.readFile(path.join(__dirname, '../public', 'error.html'), (err, data) => {
-                if (err) {
-                    res.writeHead(500);
-                    res.end('Error');
-                } else {
-                    res.writeHead(200, {
-                        'Content-Type': 'text/html'
-                    })
-                    res.end(data);
-                }
-            })
-        } else {
-            res.writeHead(200, {
-                'Content-Type': contentType
-            })
-            res.end(content);
-        }
-    })
+server.post('/', (req, res) => {
+    console.log(req.body);
+    logger(req.body.num, req.body.guess);
 });
 
-server.listen(8080, () => {
-    console.log('Server has been started on 8080...');
+server.get('/', (req, res) => {
+    fs.readFile('./server/logs/log.txt', 'utf-8', (err, data) => {
+        if (!err) {
+            res.send(data);
+        };
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server has been started on ${PORT}...`);
 });
