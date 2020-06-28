b3bbca5cc1af15eded7d1ee4e1cc37c00577eda7
kellolo <87kell@gmail.com>
2020-06-28 11:01:44 +0200

    5 add

--- PROJECT/5_Mongo/server/db/models/products.js
+++ PROJECT/5_Mongo/server/db/models/products.js
@@ -5,7 +5,8 @@ let prodSchema = new Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     img: { type: String, required: false },
-    amount: { type: Number, required: false, default: null }
+    amount: { type: Number, required: false, default: null },
+    // type:
 });
 
 module.exports = mong.model('PRODUCTS', prodSchema);
