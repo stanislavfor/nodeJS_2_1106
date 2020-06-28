17a8b0aa49fa880e85998759bf0c3365e3842af5
kellolo <87kell@gmail.com>
2020-06-28 11:00:19 +0200

    5

---
+++ PROJECT/5_Mongo/server/db/models/products.js
@@ -0,0 +1,11 @@
+let mong = require('mongoose');
+let Schema = mong.Schema;
+
+let prodSchema = new Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    img: { type: String, required: false },
+    amount: { type: Number, required: false, default: null }
+});
+
+module.exports = mong.model('PRODUCTS', prodSchema);
---
+++ PROJECT/5_Mongo/server/routers/catalog-router.js
@@ -0,0 +1,31 @@
+let express = require('express');
+let fs = require('fs');
+// let logger = require('../Utils/logger');
+let writer = require('../Utils/writer');
+let catalogServices = require('../Services/catalog');
+
+// let CatalogController = require('../controllers/catalog_controller.js');
+let { load } = require('../controllers/catalog_controller.js');
+
+
+let router = express.Router();
+
+router.get('/', load);
+
+router.post('/', (req, res) => {
+    fs.readFile('./server/db/catalog.json', 'utf-8', (err, data) => {
+        if (!err) {
+            let { newCatalog, idNewItem } = catalogServices.add(JSON.parse(data), req.body);
+            writer('./server/db/catalog.json', newCatalog)
+                .then(status => {
+                    if (status) {
+                        res.json({ id: idNewItem });
+                    } else {
+                        res.sendStatus(500);
+                    }
+                })
+        }
+    });
+});
+
+module.exports = router;
